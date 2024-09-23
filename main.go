220926f18ac4377b7b190a68a6919828bbb437a8
Paul Ghelein <paul.ghelein@gmail.com>
2024-09-23 10:53:35 +0200

    m

--- main.go
+++ main.go
@@ -11,4 +11,35 @@ si lettre non presente : message erreur et nbr essais restant
 si lettre présente, affiche toutes les lettres présentent dans le mot
 le programme continue tant que le mot n'a pas été trouvé ou que le nombre d'essaie n'est pas egale à 0
 
+hangman position :
+10 positions différentes
+	hauteur de 7 (8 avec retour a la ligne)
+
+exemple :
+$> ./hangman words.txt
+Good Luck, you have 10 attempts.
+_ _ _ _ O
+
+Choose: E
+_ E _ _ O
+
+Choose: A
+Not present in the word, 9 attempts remaining
+
+
+
+
+
+
+=========
+...
+
+
+
+func :
+- choisir le mot de manière random
+- afficher le mot et cachant des lettres random
+- lettre proposé
+- nombre restant d'essais et affichage du pendu
+
 */
