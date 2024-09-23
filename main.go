9622ed84b924f8499fe2939e654ec72f87eff94c
Paul Ghelein <paul.ghelein@gmail.com>
2024-09-23 10:42:51 +0200

    m

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package main
+
+/*
+
+Mots random dans fichier words.txt
+
+10 essais
+Lettres révélées : len(word) / 2 - 1
+
+si lettre non presente : message erreur et nbr essais restant
+si lettre présente, affiche toutes les lettres présentent dans le mot
+le programme continue tant que le mot n'a pas été trouvé ou que le nombre d'essaie n'est pas egale à 0
+
+*/
